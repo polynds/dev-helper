28c0248d529462a2ca4aceb3ae63c4c64c1bf22c
kuangjian <2910508410@qq.com>
2023-08-02 09:43:23 +0800

    keccak256

--- src/Plugin/keccak256/src/Keccak256Command.php
+++ src/Plugin/keccak256/src/Keccak256Command.php
@@ -3,12 +3,25 @@
 namespace DevHelper\Plugin\Keccak256;
 
 use DevHelper\Lib\Console\AbstractCommand;
+
 class Keccak256Command extends AbstractCommand
 {
     protected string $name = 'keccak256';
+
     public function handle()
     {
+        $str = $this->askAndValidate('请输入带加密字符串:', static function ($value) {
+            if (!$value) {
+                throw new \InvalidArgumentException(
+                    '输入错误'
+                );
+            }
+
+            return $value;
+        }, 3);
+        var_dump(Keccak256::hash($str, 256));
     }
+
     protected function configure()
     {
         $this->setDescription('keccak256');
