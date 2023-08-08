9637926edb4af9aa70617c0024f1fba8a3d8278c
kuangjian <2910508410@qq.com>
2023-08-08 09:52:23 +0800

    json & phpunit

---
+++ src/Utils/Json.php
@@ -0,0 +1,13 @@
+<?php
+
+class Json
+{
+    public static function jsonEncoder($data): string
+    {
+        $json = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return json_last_error_msg();
+        }
+        return $json;
+    }
+}
