6b98914a92f1bbcd4e9415419ae09ff2ffa125f0
kuangjian <2910508410@qq.com>
2023-08-10 10:06:18 +0800

    ReflectorClass

--- src/Lib/PHPParser/Reflector.php
+++ src/Lib/PHPParser/Reflector.php
@@ -2,10 +2,19 @@
 
 namespace DevHelper\Lib\PHPParser;
 
+use PhpParser\Node\Stmt;
 use ReflectionClass;
 
 class Reflector
 {
+    const Stmt_Class = 'Stmt_Class';
+    const Stmt_ClassConst = 'Stmt_ClassConst';
+    const Stmt_ClassMethod = 'Stmt_ClassMethod';
+    const Stmt_Interface = 'Stmt_Interface';
+    const Stmt_Namespace = 'Stmt_Namespace';
+    const Stmt_Property = 'Stmt_Property';
+
+
     private string $class;
     private array $attributes;
     private ReflectionClass $reflected;
@@ -21,9 +30,24 @@ class Reflector
     }
 
 
-    public static function getTypeName()
+    public static function getTypeName(Stmt $stmt)
     {
-
+        switch ($stmt->getType()) {
+            case self::Stmt_Class:
+                return 'Class';
+            case self::Stmt_ClassConst:
+                return 'ClassConst';
+            case self::Stmt_ClassMethod:
+                return 'ClassMethod';
+            case self::Stmt_Interface:
+                return 'Interface';
+            case self::Stmt_Namespace:
+                return 'Namespace';
+            case self::Stmt_Property:
+                return 'Property';
+            default:
+                return 'Unknown';
+        }
     }
 
     public function getAttributes(): array
