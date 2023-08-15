f27250456bb1eef684a2ef46da61e1d28ea23387
kuangjian <2910508410@qq.com>
2023-08-15 09:32:44 +0800

    test

--- tests/Keccak256Test.php
+++ tests/Keccak256Test.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use DevHelper\Plugin\Keccak256\Keccak256;
 use PHPUnit\Framework\TestCase;
 
 class Keccak256Test extends TestCase
@@ -9,6 +10,7 @@ class Keccak256Test extends TestCase
 
     public function testHash()
     {
-
+        $hash = Keccak256::hash("hello", 256);
+        self::assertEquals(Keccak256::hash("hello", 256),"1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8");
     }
 }
