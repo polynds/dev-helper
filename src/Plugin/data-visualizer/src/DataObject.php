1f50ebef161aecf8bd216eb3e2057001105da2ee
kuangjian <2910508410@qq.com>
2023-08-29 09:32:10 +0800

    data-visualizer

---
+++ src/Plugin/data-visualizer/src/DataObject.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DevHelper\Plugin\DataVisualizer;
+
+class DataObject
+{
+    protected string $data = "";
+
+    /**
+     * @return string
+     */
+    public function getData(): string
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param string $data
+     */
+    public function setData(string $data): void
+    {
+        $this->data = $data;
+    }
+
+
+}
