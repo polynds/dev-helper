bee9486224c2046c9967579d85f3be86df9f2b63
kuangjian <2910508410@qq.com>
2024-07-20 11:18:52 +0800

    new

---
+++ tests/fake/Fake8fdd871a3dde19d3.php
@@ -0,0 +1,10 @@
+<?php
+
+use DevHelper\Lib\Console\AbstractCommand;
+class Fake8fdd871a3dde19d3
+{
+    protected string $name = '8fdd871a3dde19d3';
+    public function handle()
+    {
+    }
+}
---
+++ tests/fake/Fakef3b14560d39d5606.php
@@ -0,0 +1,10 @@
+<?php
+
+use DevHelper\Lib\Console\AbstractCommand;
+class Fakef3b14560d39d5606
+{
+    protected string $name = 'f3b14560d39d5606';
+    public function handle()
+    {
+    }
+}
