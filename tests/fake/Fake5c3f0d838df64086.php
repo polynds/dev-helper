12aa94fb7f2b6a98f1af0e2829c17ada3ee3d8f7
polynds <polynds@users.noreply.github.com>
2024-09-23 14:06:34 +0800

    Update Fake5c3f0d838df64086.php

--- tests/fake/Fake5c3f0d838df64086.php
+++ tests/fake/Fake5c3f0d838df64086.php
@@ -3,7 +3,7 @@
 use DevHelper\Lib\Console\AbstractCommand;
 class Fake5c3f0d838df64086
 {
-    protected string $name = '5c3f0d838df64086';
+    protected string $name = '5c3f0d8222222222238df64086';
     public function handle()
     {
     }
