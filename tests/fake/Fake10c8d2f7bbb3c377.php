12efe092b77421afe4dd11fae1dfa52b01b23c95
kmax <kmax@qq.com>
2025-04-30 08:09:30 +0800

    new

---
+++ tests/fake/Fake10c8d2f7bbb3c377.php
@@ -0,0 +1,10 @@
+<?php
+
+use DevHelper\Lib\Console\AbstractCommand;
+class Fake10c8d2f7bbb3c377
+{
+    protected string $name = '10c8d2f7bbb3c377';
+    public function handle()
+    {
+    }
+}
---
+++ tests/fake/Fake133efea88605a5ec.php
@@ -0,0 +1,10 @@
+<?php
+
+use DevHelper\Lib\Console\AbstractCommand;
+class Fake133efea88605a5ec
+{
+    protected string $name = '133efea88605a5ec';
+    public function handle()
+    {
+    }
+}
